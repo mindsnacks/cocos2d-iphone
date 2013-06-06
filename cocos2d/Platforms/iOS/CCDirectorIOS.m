mindsnacks/cocos2d-iphone

commit b0ea9d0bb3de5714279b6eba54bad903ec350b66

Supporting landscape orientations

cocos2d/Platforms/iOS/CCDirectorIOS.m
@@ -392,11 +392,11 @@ CGFloat	__ccSoftScaleFactor = 1;
     return uiPoint;
 }
 
-// get the current size of the glview
 -(CGSize) winSize
 {
-    // Does this work for landscape?
-    return [UIScreen mainScreen].applicationFrame.size;
+    CGSize applicationFrameSize = [UIScreen mainScreen].applicationFrame.size;
+
+    return UIInterfaceOrientationIsPortrait([UIApplication sharedApplication].statusBarOrientation) ? applicationFrameSize : CGSizeMake(applicationFrameSize.height, applicationFrameSize.width);
 }
 
 -(CGSize) winSizeInPixels

commit 88b88eb12bf67fcdc79e48a83f45194cc5286dbc

Fixing landscape orientation (Broken in b0ea9d0bb3de5714279b6eba54bad903ec350b66)

cocos2d/Platforms/iOS/CCDirectorIOS.m
@@ -253,12 +253,10 @@ CGFloat	__ccSoftScaleFactor = 1;
 
 -(void) updateWinSize
 {
-    if( openGLView_ )
-    {
-        CGSize size = [openGLView_ bounds].size;
-        winSizeInPoints_ = CGSizeMake(size.width / CC_SOFT_SCALE_FACTOR(),
-                                      size.height / CC_SOFT_SCALE_FACTOR());
-    }
+    CGSize applicationFrameSize = [UIScreen mainScreen].applicationFrame.size;
+    winSizeInPoints_ = CGSizeMake(applicationFrameSize.width / CC_SOFT_SCALE_FACTOR(),
+                                  applicationFrameSize.height / CC_SOFT_SCALE_FACTOR());
+    
 	winSizeInPixels_ = CGSizeMake(winSizeInPoints_.width * CC_CONTENT_SCALE_FACTOR(),
                                   winSizeInPoints_.height * CC_CONTENT_SCALE_FACTOR());
 }
@@ -397,9 +395,15 @@ CGFloat	__ccSoftScaleFactor = 1;
 
 -(CGSize) winSize
 {
-    CGSize applicationFrameSize = [UIScreen mainScreen].applicationFrame.size;
+    CGSize s = winSizeInPoints_;
 
-    return UIInterfaceOrientationIsPortrait([UIApplication sharedApplication].statusBarOrientation) ? applicationFrameSize : CGSizeMake(applicationFrameSize.height, applicationFrameSize.width);
+	if( deviceOrientation_ == CCDeviceOrientationLandscapeLeft || deviceOrientation_ == CCDeviceOrientationLandscapeRight ) {
+		// swap x,y in landscape mode
+		CGSize tmp = s;
+		s.width = tmp.height;
+		s.height = tmp.width;
+	}
+	return s;
 }
 
 -(CGSize) winSizeInPixels